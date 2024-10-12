82bccf4eebd38629dbd467abf7ebf9d2d491cf3e
Svetlana Puchkova <pe4al.okay@gmail.com>
2024-10-12 15:37:38 +0400

    commit

--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -3,6 +3,10 @@ package pageobject;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -23,6 +27,7 @@ public  class MainPage {
     private final SelenideElement ingredients = $(byXpath(".//p[text() = 'Мясо бессмертных моллюсков Protostomia']"));
 
     private final SelenideElement enterText = $(byXpath(".//h2[text() = 'Вход']"));
+    WebDriver driver;
 
     @Step("Нажать на кнопку Булки")
     public MainPage clickBunButton() {
@@ -30,36 +35,90 @@ public  class MainPage {
         bunButton.click();
         return this;
     }
+
     @Step("Нажать на кнопку Соусы")
     public MainPage clickSauceButton() {
         sauceButton.click();
         return this;
     }
+
     @Step("Нажать на кнопку Начинки")
     public MainPage clickIngredientsButton() {
         ingredientsButton.click();
         return this;
     }
+
     @Step("Нажать на кнопку личного кабинета")
     public MainPage clickPersonalAccountButton() {
         personalAccountButton.click();
         return this;
     }
+
     @Step("Проверить переход на главную страницу")
     public MainPage checkLoginPage() {
         Assert.assertThat(enterText.getText(), containsString("Вход"));
         return this;
     }
+
     @Step("Проверить переход на вкладку Булки")
-    public Boolean checkingTransitionToBun(){
-        return fluorescentBun.isDisplayed();
+    public MainPage checkingTransitionToBun() {
+        WebElement element =
+                driver.findElement(By.xpath(".//img[@alt='Флюоресцентная булка R2-D3']"));
+
+        boolean isElementInViewport =
+                new WebDriverWait(driver, Duration.ofSeconds(1))
+                        .until(
+                                driver -> {
+                                    Rectangle rect = element.getRect();
+                                    Dimension windowSize = driver.manage().window().getSize();
+
+                                    // условие, которое проверяем внутри явного ожидания
+                                    return rect.getX() >= 0
+                                            && rect.getY() >= 0
+                                            && rect.getX() + rect.getWidth() <= windowSize.getWidth()
+                                            && rect.getY() + rect.getHeight() <= windowSize.getHeight();
+                                });
+        return this;
     }
+
     @Step("Проверить переход на вкладку Соусы")
-    public Boolean checkingTransitionToSauce(){
-        return sauce.isDisplayed();
+    public MainPage checkingTransitionToSauce(){
+        WebElement element =
+                driver.findElement(By.xpath(".//img[@alt='Соус Spicy-X']"));
+
+        boolean isElementInViewport =
+                new WebDriverWait(driver, Duration.ofSeconds(1))
+                        .until(
+                                driver -> {
+                                    Rectangle rect = element.getRect();
+                                    Dimension windowSize = driver.manage().window().getSize();
+
+                                    // условие, которое проверяем внутри явного ожидания
+                                    return rect.getX() >= 0
+                                            && rect.getY() >= 0
+                                            && rect.getX() + rect.getWidth() <= windowSize.getWidth()
+                                            && rect.getY() + rect.getHeight() <= windowSize.getHeight();
+                                });
+    return this;
     }
     @Step("Проверить переход на вкладку Начинки")
-    public Boolean checkingTransitionToIngredients() {
-        return ingredients.isDisplayed();
+    public MainPage checkingTransitionToIngredients() {
+        WebElement element =
+                driver.findElement(By.xpath(".//img[@alt='Мясо бессмертных моллюсков Protostomia']"));
+
+        boolean isElementInViewport =
+                new WebDriverWait(driver, Duration.ofSeconds(1))
+                        .until(
+                                driver -> {
+                                    Rectangle rect = element.getRect();
+                                    Dimension windowSize = driver.manage().window().getSize();
+
+                                    // условие, которое проверяем внутри явного ожидания
+                                    return rect.getX() >= 0
+                                            && rect.getY() >= 0
+                                            && rect.getX() + rect.getWidth() <= windowSize.getWidth()
+                                            && rect.getY() + rect.getHeight() <= windowSize.getHeight();
+                                });
+        return this;
     }
 }
